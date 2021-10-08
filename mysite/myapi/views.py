7d5355401665c762b79f7fd8fbf2454a11dfc630
akeemlag <akeemifedayolag@gmail.com>
2021-10-08 14:00:00 +0100

    creating a simeple api with drf commit

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 
-from .serializers import HeroSerializer
-from .models import Hero
+from .serializers import HeroSerializer, VillainSerializer
+from .models import Hero, Villain
 
 class HeroViewSet(viewsets.ModelViewSet):
     queryset = Hero.objects.all().order_by('id')
     serializer_class = HeroSerializer
+
+class VillainViewSet(viewsets.ModelViewSet):
+    queryset = Villain.objects.all().order_by('id')
+    serializer_class = VillainSerializer
