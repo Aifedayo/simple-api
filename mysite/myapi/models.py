f4989e96b2355f3947e6c29c9d01139fb26c86b9
akeemlag <akeemifedayolag@gmail.com>
2021-10-11 14:00:00 +0100

    creating a simeple api with drf commit

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -6,3 +6,10 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
+
+class Villians(models.Model):
+    name = models.CharField(max_length=60)
+    alias = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
